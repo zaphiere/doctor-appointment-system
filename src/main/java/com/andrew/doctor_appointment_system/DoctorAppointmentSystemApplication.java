7a8a076c0450cb2093833a1754112572244cd89a
zaphiere <joquin.deguzman@gmail.com>
2025-11-28 14:36:41 +0800

    Initial Commit

---
+++ src/main/java/com/andrew/doctor_appointment_system/DoctorAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.doctor_appointment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentSystemApplication.class, args);
+	}
+
+}
