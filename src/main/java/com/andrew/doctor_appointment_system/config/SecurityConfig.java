ccd35aa76efe48498a6f96dbbfa877af29beb1b6
zaphiere <joquin.deguzman@gmail.com>
2025-11-30 14:29:18 +0800

    [DOC-04]Implemented Role Based API Route Access

--- src/main/java/com/andrew/doctor_appointment_system/config/SecurityConfig.java
+++ src/main/java/com/andrew/doctor_appointment_system/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.andrew.doctor_appointment_system.enums.Role;
 import com.andrew.doctor_appointment_system.service.security.JwtFilter;
 
 @Configuration
@@ -53,6 +53,9 @@ public class SecurityConfig {
 			.authorizeHttpRequests(request -> request
 					.requestMatchers("/api/login").permitAll()
 					.requestMatchers("/api/patient/register").permitAll()
+					.requestMatchers("/api/admin/**").hasAnyRole(Role.ADMIN.name(), Role.SUPERADMIN.name())
+					.requestMatchers("/api/doctor/**").hasRole(Role.DOCTOR.name())
+					.requestMatchers("/api/patient/**").hasRole(Role.PATIENT.name())
 					.anyRequest().authenticated()
 				)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
